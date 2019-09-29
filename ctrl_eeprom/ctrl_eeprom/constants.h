00574755ab1a51cfde823c5e38e33f19af0177b9
josh_gray <gray.josh@gmail.com>
2019-09-29 10:27:02 +0200

    initial instruction encoding

--- ctrl_eeprom/ctrl_eeprom/constants.h
+++ ctrl_eeprom/ctrl_eeprom/constants.h
@@ -34,7 +34,7 @@ meaning:	AS3	AS2	AS1	AS0	ACR	AMD	AOE	--		--	--	--	--	--	--	--	--
 #define HLT	((uint8_t)1 << 19) //Halt
 #define SPE ((uint8_t)1 << 18) //Stack Pointer Enable
 #define SPW ((uint8_t)1 << 17) //Stack Pointer Write
-#define OUTW ((uint8_t)1 << 16)//Output Write
+#define OUTW ((uint8_t)1 << 16) //Output Write
 
 #define AS3 ((uint8_t)1 << 15) //ALU CTRL 3
 #define AS2 ((uint8_t)1 << 14) //ALU CTRL 2
@@ -44,8 +44,9 @@ meaning:	AS3	AS2	AS1	AS0	ACR	AMD	AOE	--		--	--	--	--	--	--	--	--
 #define AMD ((uint8_t)1 << 10) //ALU Mode
 #define AOE ((uint8_t)1 << 9)  //ALU Out Enable
 
-#define FETCH0 (MAW | PCE)
-#define FETCH1 (ME | IRW | PCC)
+//First two cycles common to all instructions
+#define FETCH0 (MAW | PCE)		//Program counter to memory address register
+#define FETCH1 (ME | IRW | PCC) //Memory to instruction register, increment program counter 
 
 inline uint8_t flip_active_low_signals(uint8_t ctrl_word)
 {
@@ -71,6 +72,7 @@ meaning:	MC	MC	MC	--	--	<---- instruction code  ---->
 #define MC_STEP6 ((uint16_t)6 << 10)
 #define MC_STEP7 ((uint16_t)7 << 10)
 
+//return step number from eeprom address
 inline uint8_t step_no(uint16_t addr)
 {
 	return addr >> 10;
@@ -87,7 +89,7 @@ Registers
 ****MOV-able registers**** 
 A	(R|W)
 B	(R|W)
-C	(R|W)
+ALO (R)
 PC	(R|W)
 OUT	(W)
  
@@ -96,13 +98,15 @@ SP	(R|W)
 FLAGS (R)
 */
 
-#define R_A 0
-#define R_B 1
-#define R_PC 2
-#define R_OUT 3
-#define R_SP 4
-#define IMM 5
-#define MEM 6
+#define R_A 1		//A Register
+#define R_B 2		//B Register
+#define R_PC 3		//Program Counter
+#define R_OUT 4		//Output
+#define R_ALO 5		//ALU Output
+#define R_SP 6		//Stack Pointer
+
+//#define IMM 6		//Immediate Value
+//#define MEM 7		//Memory Address
 
 inline uint8_t reg_read(uint8_t reg)
 {
@@ -116,6 +120,8 @@ inline uint8_t reg_read(uint8_t reg)
 		return PCE;
 	case(R_SP):
 		return SPE;
+	case(R_ALO):
+		return AOE;
 	}
 	assert(false);
 	return 0;
@@ -158,46 +164,53 @@ OR		H	H	H	L	H	X
 XOR		L	H	H	L	H	X
 */
 
-#define ALU_INC ((uint8_t) 1)
-#define ALU_DEC ((uint8_t) 2)
-#define ALU_ADD ((uint8_t) 3)
-#define ALU_ADC ((uint8_t) 4)
-#define ALU_SUB ((uint8_t) 5)
-#define ALU_SBC ((uint8_t) 6)
-#define ALU_SFT ((uint8_t) 7)
-#define ALU_CMP ((uint8_t) 8)
-#define ALU_NOT ((uint8_t) 9)
-#define ALU_AND ((uint8_t) 10)
-#define ALU_OR  ((uint8_t) 11)
-#define ALU_XOR ((uint8_t) 12)
+#define ALU_INC ((uint8_t) 0)	//Inc Bus
+#define ALU_DEC ((uint8_t) 1)	//Dec Bus
+#define ALU_ADD ((uint8_t) 2)	//Bus + B Register
+#define ALU_ADC ((uint8_t) 3)	//Bus + B Register + 1
+#define ALU_SUB ((uint8_t) 4)	//Bus - B Register
+#define ALU_SBC ((uint8_t) 5)	//Bus - B Register - 1
+#define ALU_SFT ((uint8_t) 6)	//Shift Bus Left
+#define ALU_CMP ((uint8_t) 7)	//Compare Bus to B Register
+#define ALU_NOT ((uint8_t) 8)	//!Bus
+#define ALU_AND ((uint8_t) 9)	//Bus & B Register
+#define ALU_OR  ((uint8_t) 10)	//Bus | B Register
+#define ALU_XOR ((uint8_t) 11)	//Bus ^ B Register
+//special cases
+#define ALU_PUSH ((uint8_t)12)	//Dec SP, Reg -> [SP]
+#define ALU_POP  ((uint8_t)13)	//[SP] -> Reg, Inc SP
+#define ALU_CALL ((uint8_t)14)	//Push PC, Reg -> PC
+#define ALU_RET  ((uint8_t)15)	//Pop PC
 
-inline uint8_t alu_ctrl(uint8_t aluOp)
+
+//Returns the control signals for the given operation
+inline uint32_t alu_ctrl(uint8_t aluOp)
 {
 	switch (aluOp)
 	{
 	case(ALU_INC):
-		return ((uint8_t)0);
+		return ((uint32_t)0);
 	case(ALU_DEC):
-		return ((uint8_t)AS3 | AS2 | AS1 | AS0 | ACR);
+		return ((uint32_t)AS3 | AS2 | AS1 | AS0 | ACR);
 	case(ALU_ADD):
-		return ((uint8_t)AS3 | AS0 | ACR);
+		return ((uint32_t)AS3 | AS0 | ACR);
 	case(ALU_ADC): 
-		return ((uint8_t)AS3 | AS0);
+		return ((uint32_t)AS3 | AS0);
 	case(ALU_SUB): 
-		return ((uint8_t)AS2 | AS1);
+		return ((uint32_t)AS2 | AS1);
 	case(ALU_SBC):
 	case(ALU_CMP):
-		return ((uint8_t)AS2 | AS1 | ACR);
+		return ((uint32_t)AS2 | AS1 | ACR);
 	case(ALU_SFT): 
-		return ((uint8_t)AS3 | AS2 | ACR);
+		return ((uint32_t)AS3 | AS2 | ACR);
 	case(ALU_NOT): 
-		return ((uint8_t)AMD);
+		return ((uint32_t)AMD);
 	case(ALU_AND): 
-		return ((uint8_t)AMD | AS3 | AS1 | AS0);
+		return ((uint32_t)AMD | AS3 | AS1 | AS0);
 	case(ALU_OR): 
-		return ((uint8_t)AMD | AS3 | AS2 | AS1);
+		return ((uint32_t)AMD | AS3 | AS2 | AS1);
 	case(ALU_XOR): 
-		return ((uint8_t)AMD | AS2 | AS1);
+		return ((uint32_t)AMD | AS2 | AS1);
 	}
 	assert(false);
 	return 0;
@@ -236,15 +249,40 @@ HLT
 NOP
 */
 
-inline uint8_t make_mov_instruction(uint8_t dest_reg, uint8_t src_reg)
+/*
+mov instruction encoing
+
+bit 7:		high
+bit 6:		high if src to be dereferenced
+bit 5-3:	dest register
+bit 2-0:	src register
+
+Jmp instructions are a mov into the program counter
+Immediate values are loaded by dereferencing the program counter
+
+bit:		7	6	5	4	3	2	1	0
+meaning:	1 DEREF <- DEST ->  <- SRC ->
+*/
+
+inline uint8_t make_mov_instruction(uint8_t dest_reg, uint8_t src_reg, bool deref)
 {
-	return 0;
+	return 128 | (deref << 6) | (dest_reg << 3) | src_reg;
 }
 
-inline uint8_t make_alu_instruction(uint8_t op, uint8_t dest_reg, uint8_t src_reg)
+/*
+ALU Instruction encoding
+
+bit 7:		low
+bit 6-3:	4 bit operation
+bit 2-0:	src register
+
+There are 12 ALU operations and 4 special cases (PUSH, POP, CALL, RET)
+
+bit:		7	6	5	4	3	2	1	0
+meaning :	0	<- 4 bit OP->   <- SRC ->
+
+*/
+inline uint8_t make_alu_instruction(uint8_t op, uint8_t src_reg)
 {
-	return 0;
+	return 0 | (op << 6) | src_reg;
 }
-
-
-
